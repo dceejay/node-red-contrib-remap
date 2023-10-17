123bc598147f59d09a0f85408d79b11e57561112
Dave Conway-Jones <dceejay@gmail.com>
2023-10-17 11:37:11 +0100

    much better merging of properties
    bump to 0.2.0
    (probably breaking :-)

--- remap.js
+++ remap.js
@@ -7,9 +7,10 @@ module.exports = function(RED) {
     function RemapNode(n) {
         RED.nodes.createNode(this,n);
         this.pass = n.pass || "_BLOCK_";
-        this.map = n.map;
+        this.map = n.map || {};
+        this.drop = n.drop || false;
         this.property = n.property;
-        this.outproperty = n.outproperty||n.property||"payload";
+        this.outproperty = n.outproperty??n.property;
         var node = this;
         remap[node.id] = node.map;
         node.on("input", function(msg) {
@@ -23,23 +24,55 @@ module.exports = function(RED) {
                 for (const property in value) {
                     node.map[property] = node.map[property] ?? node.pass;
                     if (node.map[property] === "_ALLOW_") {
-                        op[property] = value[property];
+                        // if existing target is an object then merge rather than overwrite
+                        if (value[property].constructor.name === "Object") {
+                            op[property] = { ...op[property], ...value[property] };
+                        }
+                        else { op[property] = value[property]; }
                     }
                     else if (node.map[property] !== "_BLOCK_") {
-                        op[node.map[property]] = value[property];
+                        // if existing target is an object then merge rather than overwrite
+                        if (value[property].constructor.name === "Object") {
+                            op[node.map[property]] = { ...op[node.map[property]], ...value[property] };
+                        }
+                        else { op[node.map[property]] = value[property]; }
                     }
                 }
+                remap[node.id] = node.map;
+                // if resulting object is empty and we said to dropall then pass nothing on
+                if (node.drop == true && Object.keys(op).length === 0) { return; }
+                // as long as the input property isn't the whole msg then
                 if (typeof node.property === "string" && node.property.length > 0) {
-                    RED.util.setMessageProperty(msg,node.outproperty,op);
+                    // if we are targetting top level msg
+                    if (node.outproperty === "") {
+                        RED.util.setMessageProperty(msg,node.property,undefined);
+                        const tmp = {...RED.util.cloneMessage(msg), ...op};  //merge
+                        node.send(tmp);
+                        return;
+                    }
+                    // if out prop is != input property then merge into any existing output property
+                    // and delete incoming property
+                    if (node.outproperty !== node.property) {
+                        const tmp = RED.util.getMessageProperty(msg,node.outproperty) || {};
+                        RED.util.setMessageProperty(msg,node.outproperty,{ ...tmp, ...op });
+                        RED.util.setMessageProperty(msg,node.property,undefined);
+                    }
+                    // otherwise just replace the existing property
+                    else { RED.util.setMessageProperty(msg,node.outproperty,op); }
                     node.send(msg);
                 }
                 else {
+                    // If the input property is the whole msg then just replace the msg
                     op._msgid = msg._msgid;  // re-attach original _msgid
                     node.send(op);
                 }
             }
             else { node.send(msg); } // if no property - just pass it on.
         });
+
+        node.on("close", function() {
+            delete remap[node.id];
+        })
     }
     RED.nodes.registerType("remap",RemapNode);
 
